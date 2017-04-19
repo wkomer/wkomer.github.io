3f5f941d90d3270a2f010d271764823da95e4c94
wkomer <euro_driver@live.com>
2017-04-19 17:12:12 -0400

    reverting to old one after this

--- js/demo-1.js
+++ js/demo-1.js
@@ -88,4 +88,16 @@ window.requestAnimFrame = (function(){
   draw();
   requestAnimFrame(loop);
 })();    
+
+function resize(){    
+    $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
+  }
+
+  $(document).ready(function(){
+    resize();
+    $(window).on("resize", function(){                      
+        resize();
+    });
+      
+  });
     
